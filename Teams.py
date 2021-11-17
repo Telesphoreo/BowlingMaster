d970590ab3156ed127fb0da12db57da0f92a308d
Telesphoreo <me@telesphoreo.me>
2021-11-17 16:04:16 -0600

    Start teams

--- Teams.py
+++ Teams.py
@@ -1,7 +1,26 @@
 import csv
+from os.path import exists
+
+filename = "teams.txt"
+
 
 class Teams:
     def __init__(self, playerID):
         self.playerID = playerID
+        self.initializeFile()
 
-    def createTeam(self):
+    def initializeFile(self):
+        # Create the file if it doesn't exist already as a failsafe
+        if not exists(filename):
+            with open(filename, "x") as file:
+                file.close()
+
+    def createTeam(self, team_name):
+        with open(filename, "a") as file:
+            file.write(team_name + "\n")
+
+
+def main():
+    Teams("not yet implemented").createTeam("test team")
+
+main()
