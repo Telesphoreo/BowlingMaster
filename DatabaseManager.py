6ede6a9c2f48ff17c6eae70e9155ec266da8aef2
Telesphoreo <me@telesphoreo.me>
2021-11-17 20:10:28 -0600

    Rough implementation of scores

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -1,8 +1,13 @@
+import re
 from prettytable import from_db_cursor
 import sqlite3
 
 connection = sqlite3.connect("players.db")
 cursor = connection.cursor()
+
+
+def stripTuple(string):
+    return re.sub("[^0-9]", "", str(string))
 
 
 class Player:
@@ -34,10 +39,13 @@ class Player:
         return list
 
     def getName(self, playerID):
-        rows = cursor.execute(
-            "SELECT name FROM players WHERE id = ?",
-            (playerID,)).fetchall()
-        return rows
+        try:
+            result = cursor.execute(
+                "SELECT name FROM players WHERE id = ?",
+                (playerID,)).fetchall()
+            return stripTuple(result)
+        except IndexError:
+            print("No player with that ID could be found.")
 
 
 class Teams:
@@ -47,7 +55,7 @@ class Teams:
     # Initialize the teams table
     def initializeDatabase(self):
         tb_exists = "SELECT name FROM sqlite_master WHERE type='table' AND name='teams'"
-        if not cursor.execute(tb_exists).fetchone():
+        if not cursor.execute(tb_exists):
             cursor.execute(
                 "CREATE TABLE teams (name TEXT)")
         connection.commit()
@@ -76,11 +84,49 @@ class Teams:
         cursor.execute("UPDATE players SET team = NULL WHERE id = ?", (playerID,))
         connection.commit()
 
+
 class Points:
-    def __init__(self, pnts, strikes):
-        self.pnts = pnts
+    def __init__(self, playerID, points, strikes):
+        self.playerID = playerID
+        self.points = points
         self.strikes = strikes
 
-    def getScores(self):
-        pnts = int(input("Enter your total score."))
-        strikes = int(input("Enter your number of total strikes."))
+    def setScore(self):
+        # Fetch strikes and total score
+        db_strikes = cursor.execute(
+            "SELECT strikes_per_game FROM players WHERE id = ?",
+            (self.playerID,)).fetchall()
+        db_total = cursor.execute(
+            "SELECT total_score FROM players WHERE id = ?",
+            (self.playerID,)).fetchall()
+
+        # Strip tuple from operation
+        strip_strikes = stripTuple(db_strikes)
+        strip_total = stripTuple(db_total)
+        print(strip_strikes)
+
+        # Probably need additional checks here
+        if strip_strikes == "":
+            strip_strikes = 0
+        if int(self.strikes) > 12:
+            raise ValueError("The amount of strikes cannot be more than 12!")
+        if strip_total == "":
+            strip_total = 0
+        if int(self.points) > 300:
+            raise ValueError("The score cannot be more than 300!")
+
+        # Add more to current strikes and total score
+        new_strikes = int(strip_strikes) + int(self.strikes)
+        new_total = int(strip_total) + int(self.points)
+
+        # Execute operation
+        cursor.execute("UPDATE players SET strikes_per_game = ?, total_score = ? WHERE id = ?",
+                       (new_strikes, new_total, self.playerID))
+        connection.commit()
+
+
+def main():
+    Points(1, 300, 12).setScore()
+
+
+main()
