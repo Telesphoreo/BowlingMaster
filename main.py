3c908c36fb950100a0a80ece688f9ed59f5b6e30
ErroRr69 <raystin.grimes@gmail.com>
2021-11-21 14:47:38 -0600

    Gooewey stuff

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from DatabaseManager import Teams
 from DatabaseManager import Points
 import DateManager
 from datetime import date
-
+import tkinter as tk
+from tkinter import ttk, Entry, StringVar
 
 def displayManageTeamsWindow():
     print("1 - Add Team")
@@ -172,6 +173,25 @@ def main():
             main()
             break
 
+class MyFrame(ttk.Frame):
+    def __init__(self, parent):
+        ttk.Frame.__init__(self, parent, padding="10 10 10 10")
+        self.pack()
+        ttk.Button(self, text="Manage Teams").grid(column=0, row=0, sticky=tk.E)
+        ttk.Button(self, text="Manage Players").grid(column=0, row=1, sticky=tk.E)
+        ttk.Button(self, text="Manage Scores").grid(column=0, row=2, sticky=tk.E)
+        ttk.Button(self, text="Other Options").grid(column=0, row=3, sticky=tk.E)
+        ttk.Button(self, text="Show All Players").grid(column=0, row=4, sticky=tk.E)
+        ttk.Button(self, text="About").grid(column=0, row=5, sticky=tk.E)
+        ttk.Button(self, text="Exit").grid(column=0, row=6, sticky=tk.E)
+
+        for child in self.winfo_children():
+            child.grid_configure(padx=5, pady=3)
 
 if __name__ == "__main__":
-    main()
+    root = tk.Tk()
+    root.title("Bowling Master")
+    MyFrame(root)
+    root.mainloop()
+
+
