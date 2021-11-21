ef7f373382e651abe7b8a81ae964ae10d4280d3c
ErroRr69 <raystin.grimes@gmail.com>
2021-11-21 14:43:32 -0600

    Gooewey stuff

--- guiFile.py
+++ guiFile.py
@@ -1,42 +1,24 @@
 import tkinter as tk
-from tkinter import ttk, Entry, Label, StringVar
+from tkinter import ttk, Entry, StringVar
 
 class MyFrame(ttk.Frame):
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent, padding="10 10 10 10")
         self.pack()
-
-
-        self.miles = tk.StringVar()
-        self.gallons = tk.StringVar()
-        self.mpg = tk.StringVar()
-
-        ttk.Label(self, text="Miles Driven:").grid(
-            column=0, row=0, sticky=tk.E)
-        ttk.Label(self, text="Gallons Used:").grid(
-            column=0, row=1, sticky=tk.E)
-        ttk.Label(self, text="MPG:").grid(
-            column=0, row=2, sticky=tk.E)
-        ttk.Entry(self, width=30, textvariable=self.miles).grid(
-            column=1, row=0)
-        ttk.Entry(self, width=30, textvariable=self.gallons).grid(
-            column=1, row=1)
-        ttk.Entry(self, width=30, textvariable=self.mpg, state = "readonly").grid(
-            column=1, row=2)
-        ttk.Button(self, text="Calculate", command=self.calculator).grid(column=1, row=3, sticky=tk.E)
+        ttk.Button(self, text="Manage Teams").grid(column=0, row=0, sticky=tk.E)
+        ttk.Button(self, text="Manage Players").grid(column=0, row=1, sticky=tk.E)
+        ttk.Button(self, text="Manage Scores").grid(column=0, row=2, sticky=tk.E)
+        ttk.Button(self, text="Other Options").grid(column=0, row=3, sticky=tk.E)
+        ttk.Button(self, text="Show All Players").grid(column=0, row=4, sticky=tk.E)
+        ttk.Button(self, text="About").grid(column=0, row=5, sticky=tk.E)
+        ttk.Button(self, text="Exit").grid(column=0, row=6, sticky=tk.E)
 
         for child in self.winfo_children():
             child.grid_configure(padx=5, pady=3)
-    def calculator(self):
-        miles = float(self.miles.get())
-        gallons = float(self.gallons.get())
 
-        milespGallon = miles/gallons
-        milespGallon = round(milespGallon, 2)
-        self.mpg.set(milespGallon)
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("MPG Calculator")
+    root.title("Bowling Master")
     MyFrame(root)
     root.mainloop()
 
